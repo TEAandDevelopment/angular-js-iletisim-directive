c74f9dc2a37ea1af59a016db8740a80aedc2bbbe
TEAandDevelopment <teaanddevelopment@gmail.com>
2015-04-09 10:45:03 +0300

    proje oluşturulması

---
+++ src/_assets/js/main.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp', []);
+
+// controllers
+myApp.controller('myController', ['$scope',
+    function ($scope) {
+
+        $scope.scopeNesnesi = {
+            tamIsim: "Talha Emin AYDEMİR",
+            ePosta: "talhaeminaydemir@gmail.com",
+            website: "https://www.facebook.com/teaprogrammer",
+            dogumTarihi: "10-07-1996"
+        };
+
+    }
+]);
+
+myApp.directive("iletisimBilgilerim", function () {
+    return {
+        restrict: "A",
+        templateUrl: "views/partial/contact.html"
+    };
+});
